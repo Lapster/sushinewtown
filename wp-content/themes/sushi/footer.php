6d617d13cd779342f501015ff134b893f06a5d5b
Ngo, Lap Thanh (CA - Montreal) <lango@deloitte.ca>
2017-09-26 00:03:35 -0400

    Change Postal Code

--- wp-content/themes/sushi/footer.php
+++ wp-content/themes/sushi/footer.php
@@ -6,7 +6,7 @@
 			<div>
 				<a href="https://www.google.ca/maps/place/Sushi+Newtown/@45.4812719,-73.5855859,13z/data=!4m8!1m2!2m1!1ssushi+newtown!3m4!1s0x4cc91a6110245d2f:0x30b2c77e46f41eb!8m2!3d45.492231!4d-73.562602" target="_blank">
 					<p>311 rue De La Montagne</p>
-					<p>Montreal, Quebec, QC H3C 2B2</p>
+					<p>Montreal, Quebec, QC H3C 0R6</p>
 				</a>
 				<a href="tel:514-379-4788">
 					<h4>514-379-4788</h4>
